1c1feea00ae3107cfc6a7335f04709894407be5b
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-06-02 21:17:33 +0900

    snake directions

--- snake.py
+++ snake.py
@@ -3,6 +3,7 @@ from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
+
 class Snake:
 
     def __init__(self):
@@ -23,3 +24,15 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.segments[0].setheading(90)
+
+    def down(self):
+        self.segments[0].setheading(270)
+
+    def left(self):
+        self.segments[0].setheading(180)
+
+    def right(self):
+        self.segments[0].setheading(0)
