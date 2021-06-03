09fb159bbc7ec362f227c61afaa85f3c404de868
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-06-03 16:07:31 +0900

    game over

--- main.py
+++ main.py
@@ -27,10 +27,14 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # Detect Collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
-        scoreboard.points += 1
-        scoreboard.clear()
-        scoreboard.score()
+        scoreboard.increase_score()
+
+    # Detect Collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,14 +1,25 @@
 from turtle import Turtle
-
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.hideturtle()
-        self.goto((0, 280))
+        self.score = 0
         self.color('white')
-        self.points = 0
-        self.score()
+        self.penup()
+        self.goto((0, 270))
+        self.hideturtle()
+        self.update_scoreboard()
 
-    def score(self):
-        self.write(f"Score: {self.points}", align="center", font=("Arial", 15, "normal"))
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
